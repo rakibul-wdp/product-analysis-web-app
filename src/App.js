a0ff79bffd64ef1da66cfc02de32d440399ae55b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-04 15:48:51 +0600

    create some component and install react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/reviews' element={<Reviews/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>This is also Dashboard for rechart</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
