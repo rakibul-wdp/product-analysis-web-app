5aa8c86780db26a432f374b289ab6a74d89922ef
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-05 14:39:47 +0600

    finish blog page style

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
   return (
-    <div>
-      <div>
-        <h2>What is Context Api...???</h2>
-        <p>
+    <div className='blog-container'>
+      <div className='single-blog-container'>
+        <h2 className='blog-question'>What is Context Api...???</h2>
+        <p className='blog-para'>
           Context is another way of props drilling. Also it's more than powerful related to props drilling. Context API
           is a way for a React app to effectively produce global variables that can be passed around or any where in
           react components. When we use prop drilling like a tree or parent to child components for passing data one
@@ -18,15 +19,15 @@ const Blogs = () => {
           of the tree.
         </p>
       </div>
-      <div>
-        <h2>What is Semantic Elements or Tags...???</h2>
-        <p>In one sentence, semantic elements means a element with a meaning. What is meaning? Meaning means when we use semantic element it's tell us which type of code or which type of work doing in this element. A semantic element easily and clearly describe its works or meaning to a developer and also browser. Let's see example. If we use use div or span tag, it's tells nothing about in this content. But when we use semantic element like header, nav main, footer etc. its clearly describe about its content. Some example with case wise. Section element use for chapters, introduction, contact information. Article element used for blog posts, user comments, product cards. Footer is more than clear element, its use for copyright info, contact info, authorship info. Nav tag use in header tag for set of navigation links. Other semantic element is article, aside, details, header, main, summary etc. So semantic element are clearly describe their meaning in a developer and also machine. </p>
+      <div className='single-blog-container'>
+        <h2 className='blog-question'>What is Semantic Elements or Tags...???</h2>
+        <p className='blog-para'>In one sentence, semantic elements means a element with a meaning. What is meaning? Meaning means when we use semantic element it's tell us which type of code or which type of work doing in this element. A semantic element easily and clearly describe its works or meaning to a developer and also browser. Let's see example. If we use use div or span tag, it's tells nothing about in this content. But when we use semantic element like header, nav main, footer etc. its clearly describe about its content. Some example with case wise. Section element use for chapters, introduction, contact information. Article element used for blog posts, user comments, product cards. Footer is more than clear element, its use for copyright info, contact info, authorship info. Nav tag use in header tag for set of navigation links. Other semantic element is article, aside, details, header, main, summary etc. So semantic element are clearly describe their meaning in a developer and also machine. </p>
       </div>
-      <div>
-        <h2>About Inline, Block, Inline-block element</h2>
-        <p>Block-level elements consume full width of available. It starts new line and add some space before and after element. Block elements example are h1 to h6, p, div, ul etc.</p>
-        <p>Inline elements contain exact their content. It doesn't start new line. It takes as much width as necessary. In inline element can't set width or height.</p>
-        <p>Inline-block elements just like inline element, where it doesn't start new line. But it take width and height value. It is the main difference between inline and inline-block elements.</p>
+      <div className='single-blog-container'>
+        <h2 className='blog-question'>About Inline, Block, Inline-block element</h2>
+        <p className='blog-para'>Block-level elements consume full width of available. It starts new line and add some space before and after element. Block elements example are h1 to h6, p, div, ul etc.</p>
+        <p className='blog-para'>Inline elements contain exact their content. It doesn't start new line. It takes as much width as necessary. In inline element can't set width or height.</p>
+        <p className='blog-para'>Inline-block elements just like inline element, where it doesn't start new line. But it take width and height value. It is the main difference between inline and inline-block elements.</p>
       </div>
     </div>
   );
